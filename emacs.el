7212442fa4e7d9080db0dd5aa74388a34ca285c6
Marcel Kapfer (mmk2410) <marcelmichaelkapfer@yahoo.co.nz>
2016-12-04 23:19:35 +0100

    Extended emacs config

--- emacs.el
+++ emacs.el
@@ -1,5 +1,5 @@
 (setq user-full-name "Marcel Kapfer (mmk2410")
-(setq user-mail-address "marcelmichaelkapfer@yahoo.co.nz")
+(setq user-mail-address "marcelmichaelkapfer@gmail.com")
 
 (load "package")
 (package-initialize)
@@ -32,3 +32,69 @@
 ;; Save backup files in .emacs
 (setq auto-save-file-name-transforms
       `((".*" ,(concat user-emacs-directory "auto-save/") t )))
+
+;; Mutt support.
+(setq auto-mode-alist (append '(("/tmp/mutt.*" . mail-mode)) auto-mode-alist))
+
+;; auctex
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-save-query nil)
+(setq TeX-PDF-mode t)
+
+;; outline mode
+
+(defun turn-on-outline-minor-mode ()
+  (outline-minor-mode 1))
+
+(add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
+(add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
+(setq outline-minor-mode-prefix "\C-c \C-o") ; Or something else
+
+;; flymake for latex
+(defun flymake-get-tex-args (file-name)
+  (list "chktex" (list "-q" "-v0" file-name)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (atom-one-dark)))
+ '(custom-safe-themes
+   (quote
+    ("1160f5fc215738551fce39a67b2bcf312ed07ef3568d15d53c87baa4fd1f4d4e" default)))
+ '(send-mail-function (quote smtpmail-send-it)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; edit-server
+
+(require 'edit-server)
+(edit-server-start)
+
+
+
+; mmk2410 emacs configuration
+
+;; variabels
+
+;;; always follow symlinks to git repos
+(setq vc-follow-symlinks t)
+
+;;; set default column width
+(setq-default fill-column 80)
+
+
+;; packages
+
+;;; fill-column indicator
+
+(require 'fill-column-indicator)
+(setq fci-rule-width 5) ;;; set rule width to 5px
+(add-hook 'after-change-major-mode-hook 'fci-mode) ;; enable fci on every file
+
