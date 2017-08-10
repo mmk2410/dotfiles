6351db2aea820be1946152a2dad129432f7f6855
Marcel Kapfer <marcelmichaelkapfer@gmail.com>
2017-08-10 09:54:02 +0200

    emacs: switched to fira code as font

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -71,7 +71,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hermit" :foundry "unknown" :slant normal :weight normal :height 90 :width normal))))
+ '(default ((t (:inherit nil :stipple nil :background "#21242b" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :family "Fira Code"))))
  '(cursor ((t (:background "light gray")))))
 
 
