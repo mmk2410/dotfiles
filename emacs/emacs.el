29e97cb0d797f2e3620aa4ea2d807f1cb3ace240
Marcel Kapfer <opensource@mmk2410.org>
2017-10-06 19:43:40 +0200

    Emacs: Enabled delete-selection-mode and basic ivy config

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -100,6 +100,9 @@
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 
+;;; delete selection mode
+(delete-selection-mode 1)
+
 ;;; window / frame size
 (setq initial-frame-alist
       '(
@@ -114,7 +117,6 @@
 	))
 
 ;;; indention
-
 (setq-default indent-tabs-mode nil)
 (setq tab-width 2)
 
@@ -325,3 +327,30 @@
 
 ;;; enable neotree theme
 (doom-themes-neotree-config)
+
+
+;; experimental stuff
+
+;;; ivy default configuration
+
+(ivy-mode 1)
+
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
