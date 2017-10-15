7efbb6a7ff2e1340e8ef4655d47d6ff155bda977
Marcel Kapfer <opensource@mmk2410.org>
2017-10-15 16:49:01 +0200

    fish: added some git aliases

--- shells/fish/functions/g.fish
+++ shells/fish/functions/g.fish
@@ -1,3 +1,27 @@
 function g
-    git $argv
+        git $argv
+end
+
+function gst
+        git status $argv
+end
+
+function gcom
+        git commit $argv
+end
+
+function gad
+        git add $argv
+end
+
+function gup
+        git push $argv
+end
+
+function gdo
+        git pull $argv
+end
+
+function gdif
+        git diff $argv
 end
