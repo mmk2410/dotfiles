f0a248c476b91c7d22b44483826ebb40096932b6
Marcel Kapfer <opensource@mmk2410.org>
2018-08-03 09:21:51 +0200

    Emacs: redefined treemacs config

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -548,44 +548,29 @@
 ;; A tree style file explorer package
 (use-package treemacs
   :init
-  (define-prefix-command 'treemacs-map)
-  (global-set-key (kbd "M-m") 'treemacs-map)
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (setq treemacs-follow-after-init          t
-        treemacs-width                      35
-        treemacs-indentation                2
-        treemacs-git-integration            t
-        treemacs-collapse-dirs              (if (executable-find "python") 3 0)
-        treemacs-silent-refresh             nil
-        treemacs-change-root-without-asking nil
-        treemacs-sorting                    'alphabetic-desc
-        treemacs-show-hidden-files          t
-        treemacs-never-persist              nil
-        treemacs-is-never-other-window      nil
-        treemacs-goto-tag-strategy          'refetch-index)
+  (setq treemacs-is-never-other-window t
+        treemacs-project-follow-cleanup t
+        treemacs-python-executable (executable-find "python3")
+        treemacs-recenter-after-file-follow t)
   (treemacs-follow-mode t)
-  (treemacs-filewatch-mode t)
+  (treemacs-git-mode 'extended)
+  (treemacs-filewatch-mode)
   :bind
   (:map global-map
-        ("<f9>"         . treemacs-toggle)
-        ("M-0"        . treemacs-select-window)
-        ("C-c 1"      . treemacs-delete-other-windows)
-        ("M-m ft"     . treemacs-toggle)
-        ("M-m fT"     . treemacs)
-        ("M-m fB"     . treemacs-bookmark)
-        ("M-m f C-t"  . treemacs-find-file)
-        ("M-m f M-t" . treemacs-find-tag)))
+	("M-0" . treemacs-select-window)
+	("C-x t 1" . treemacs-delete-other-window)
+	("C-x t t" . treemacs)
+	("C-x t B" . treemacs-bookmark)
+	("C-x t C-t" . treemacs-find-file)
+	("C-x t M-t" . treemacs-find-tag)))
 
 ;; treemacs-projectile
 ;; Projectile integration for treemacs
 (use-package treemacs-projectile
-  :after treemacs
-  :config
-  (setq treemacs-header-function #'treemacs-projectile-create-header)
-  :bind
-  (:map global-map
-        ("M-m fP" . treemacs-projectile)
-        ("M-m fp" . treemacs-projectile-toggle)))
+  :after treemacs projectile)
 
 ;; magit
 ;; A Git porcelain inside Emacs
