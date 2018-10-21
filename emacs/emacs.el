f86154d391a75961b0663ace2f5b26b49bb13c96
Marcel Kapfer <opensource@mmk2410.org>
2018-10-21 13:37:36 +0200

    Emacs: beginend dropped support for Emacs 25.1, using old version

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -697,6 +697,7 @@
 ;; beginend
 ;; Redefine M-< and M-> for some modes
 (use-package beginend
+  :load-path "beginend/"
   :diminish (beginend-global-mode
 	     beginend-prog-mode
 	     beginend-magit-status-mode)
