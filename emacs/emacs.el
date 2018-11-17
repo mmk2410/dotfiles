dd19e3c74af9f42f34d74cc91fb95344b735869a
Marcel Kapfer <opensource@mmk2410.org>
2018-11-17 06:29:10 +0100

    Emacs: removed nord-theme (alreday commented out)

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1379,18 +1379,6 @@ signature-file is the path to the file which contains the signature."
   :after spaceline
   :config (spaceline-all-the-icons-theme))
 
-;; Nord theme
-;; https://github.com/arcticicestudio/nord-emacs
-;; (use-package nord-theme
-;;   :config
-;;   ;; fixed shitty Emacs behavior
-;;   ;; fix for theme not loading in deamon mode
-;;   (if (daemonp)
-;;       (add-hook 'after-make-frame-functions
-;; 		(lambda (frame)
-;; 		  (with-selected-frame frame (load-theme 'nord t))))
-;;     (load-theme 'nord t)))
-
 ;; Doom theme
 ;; https://github.com/hlissner/emacs-doom-themes
 (use-package doom-themes
