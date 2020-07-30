2709f26a14f7a59b876366926069d3ce21e3b263
Marcel Kapfer <opensource@mmk2410.org>
2020-07-30 10:22:55 +0200

    [qtile] Apply floating rules to pinentry-gtk2

--- qtile/config.py
+++ qtile/config.py
@@ -224,6 +224,7 @@ floating_layout = layout.Floating(float_rules=[
     {'wmclass': 'maketag'},  # gitk
     {'wname': 'branchdialog'},  # gitk
     {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'pinentry-gtk-2'},  # GTK+2 GPG password entry
     {'wmclass': 'ssh-askpass'},  # ssh-askpass
 ])
 auto_fullscreen = True
